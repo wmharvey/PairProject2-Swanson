41f5e3a33265739aba73103edbf7750034baa6ff
Miranda Raymond <mirandaraymond@me.com>
2015-11-16 14:51:29 -0800

    added contact and comment form, still need to add local storage

--- JS/contactform.js
+++ JS/contactform.js
@@ -1,20 +1,65 @@
+//Contact Form
+
+var Contact = function (firstName, lastName, concern) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.concern = concern;
+}
+
+var concernForm = document.getElementById('contactform');
+var concernSubmit = function() {
+  event.preventDefault();
+
+  if (!event.target.firstname.value || !event.target.lastname.value || !event.target.concern.value) {
+    return alert ('You must fill in all the fields');
+  }else {
+    return alert ('Thank you for your input! Someone will contact you within the next 24 hours.');
+  }
+
+  var first = event.target.firstname.value;
+  var last = event.target.lastname.value;
+  var concern = event.target.concern.value;
+
+  var newContact = new Contact(first, last, concern);
+  console.log('Comment by ' + event.target.firstname.value + event.target.lastname + ' says ' + event.target.concern.value);
+  event.target.firstname.value = null;
+  event.target.lastname.value = null;
+  event.target.concern.value = null;
+}
+
+concernForm.addEventListener('submit', concernSubmit);
+
+//Comment Form
 var Comment = function(userName, text) {
   this.userName = userName;
   this.text = text;
-
-  var paragraph = document.createElement('p');
-  p.innerHTML = this.text + " - " + this.userName;
-  return p;
 }
 
-var comments = document.getElementById('comment-display');
-var chatForm = document.getElementById('commentform');
+Comment.prototype.render = function() {
+  var li = document.createElement('li');
+  li.innerHTML = "\"" + this.text + "\"" + " - " + this.userName;
+  return li;
+};
+
+var comments = document.getElementById('commentdisplay');
+var commentForm = document.getElementById('commentform');
 var commentData = [];
+
+var Leslie = new Comment('Leslie Knope', 'Ron is a poetic noble land mermaid.');
+commentData.push(Leslie);
+var Tom = new Comment('Tom Haverford', 'Entertainment720 will be hosting all the dope ass parties in the White House if Ron gets elected.');
+commentData.push(Tom);
+var Andy = new Comment('Andy Dwyer', 'Ron gave me the best advice I ever received. \'Never half-ass two things. Whole-ass one thing.\'');
+commentData.push(Andy);
+var Jerry = new Comment('Jerry/Gary/Larry Gergich', 'Ron has cried twice in his life. Once, when he was 7 and was hit by a bus, and again when he learned that L\'il Sebastian had passed. That\'s the kind of man I\'d like to see as President.');
+commentData.push(Jerry);
+var Burt = new Comment('Burt Macklin, FBI','Burt Macklin. FBI. You thought I was dead? So did the President\'s enimies.');
+commentData.push(Burt);
 
 var renderAllComments = function() {
   comments.innerHTML = '';
   commentData.forEach(function(comment) {
-    comments.appendChild(comment.render());
+    commentdisplay.appendChild(comment.render());
   });
 };
 
@@ -23,11 +68,11 @@ var commentSubmit = function(event) {
   event.preventDefault();
 
   if (!event.target.name.value || !event.target.comment.value) {
-    return alert ('You must fill in all the fields)');
-
-    var commenter = event.target.name.value;
-    var submission = event.target.comment.value;
+    return alert ('You must fill in all the fields');
   }
+
+  var commenter = event.target.name.value;
+  var submission = event.target.comment.value;
 
   var newComment = new Comment(commenter, submission);
   console.log('Comment by ' + event.target.name.value + ' at ' + Date());
@@ -38,7 +83,5 @@ var commentSubmit = function(event) {
   renderAllComments();
 };
 
-chatForm.addEventListener('submit', handleCommentSubmit);
-
-
-
+renderAllComments();
+commentForm.addEventListener('submit', commentSubmit);
